a78f253de61ef902a7801f693399dc07ab5ad554
chendi <lumblessingc@gmail.com>
2024-02-06 08:33:21 +0100

    added: arithmetic operators

---
+++ arithmetic_operators/index.js
@@ -0,0 +1,5 @@
+// arithmetic operators = operands (values, variables,etc )
+//                        operators (+ - * / )
+let result = 1 + 2 * 3 + 4 ** 2;
+console.log(result);
+document.getElementById("p1").textContent = `the result to the following equation 1 + 2 * 3 + 4 ** 2 is: ${result}`;
